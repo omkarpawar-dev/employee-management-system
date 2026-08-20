3f5416bd1d86840a1f55cfa7e67f1fdab2b5aa47
omkar <omkarpawar20052@gmail.com>
2026-08-20 17:19:09 +0530

    Create Employee model with getters and setters

---
+++ src/model/Employee.java
@@ -0,0 +1,70 @@
+package model;
+
+public class Employee {
+
+        private int employeeId;
+        private String name;
+        private int age;
+        private String email;
+        private String department;
+        private double salary;
+
+        public Employee(int employeeId, String name, int age,String email,String department, double salary){
+            this.employeeId=employeeId;
+            this.name=name;
+            this.age=age;
+            this.email=email;
+            this.department=department;
+            this.salary=salary;
+        }
+
+        public void setEmployeeId(int employeeId){
+            this.employeeId=employeeId;
+        }
+
+        public void setName(String name){
+            this.name=name;
+        }
+
+        public void setAge(int age){
+            this.age=age;
+        }
+
+        public void setEmail(String email){
+            this.email=email;
+        }
+
+        public void setDepartment(String department){
+            this.department=department;
+        }
+        public void setSalary(double salary){
+            this.salary=salary;
+        }
+
+        public int getEmployeeId(){
+            return employeeId;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public int getAge(){
+            return age;
+        }
+
+        public String getEmail(){
+            return email;
+        }
+
+        public String getDepartment(){
+            return department;
+        }
+
+        public double getSalary(){
+            return salary;
+        }
+
+}
+
+
