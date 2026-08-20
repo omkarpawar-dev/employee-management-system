2bceaf7f3c839d3e06443e4291edebf7b905860a
omkar <omkarpawar20052@gmail.com>
2026-08-20 19:14:32 +0530

    Add EmployeeService with employee storage and add method

--- src/service/EmployeeService.java
+++ src/service/EmployeeService.java
@@ -1,4 +1,13 @@
 package service;
+import model.Employee;
+
+import java.util.ArrayList;
 
 public class EmployeeService {
+    private ArrayList<Employee> employees=new ArrayList<>();
+
+    public void addEmployee(Employee employee){
+        employees.add(employee);
+        System.out.println("Employee added successfully");
+    }
 }
